1209fe04b3081917ad890d95956b36f54650f729
Gopal Nyaupane <gopal.nyaupane@dishhome.com.np>
2025-02-23 22:22:18 +0545

    update

--- JobPortalApplication/Controllers/EducationalQualificationsController.cs
+++ JobPortalApplication/Controllers/EducationalQualificationsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using JobPortalApplication.Data;
 using JobPortalApplication.Models;
+using NuGet.Packaging;
 
 namespace JobPortalApplication.Controllers
 {
